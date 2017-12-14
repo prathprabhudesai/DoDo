79008ed96cfd0fdc5c4a689ab734e3ddcf74b6b8
mrunmayeejog <mrunmayee.jog@xoriant.com>
2017-12-14 13:04:04 +0530

    Save tasks in sorted order based on status and priority

--- todo.py
+++ todo.py
@@ -38,10 +38,21 @@ def todo_add():
     task['DEADLINE'] = deadline
     task['PRIORITY'] = "[#" + priority.upper() + "]"
     todos.append(task)
+    save_tasks(todos)
+    sys.stdout.write("" + bcolors.WARNING + "Task Added! \n" + bcolors.ENDC)
+
+def save_tasks(todos_list = None):
+    tasks = []
+    pending_tasks = [p_task for p_task in todos_list if p_task['STATUS'] == 'TODO']
+    done_tasks = [d_task for d_task in todos_list if d_task['STATUS'] == 'DONE']
+    pending_tasks = sorted(pending_tasks, key=lambda k: k['PRIORITY'])
+    done_tasks = sorted(done_tasks, key=lambda k: k['PRIORITY'])
+    tasks.extend(pending_tasks)
+    tasks.extend(done_tasks)
     with open(source_file, 'w') as fp:
-        json.dump(todos, fp)
-    sys.stdout.write("" + bcolors.WARNING + "Task Added! \n")
-        
+        json.dump(tasks, fp)
+
+
 def todo_del():
     todo_list()
     try:
@@ -57,16 +68,14 @@ def todo_del():
         if task['NAME'] == del_task[0]:
             break
     todos.remove(task)
-    with open(source_file, 'w') as fp:
-        json.dump(todos, fp)
-    sys.stdout.write("" + bcolors.WARNING + "Succcessfully deleted the task :-)\n")
-    
+    save_tasks(todos)
+    sys.stdout.write("" + bcolors.WARNING + "Succcessfully deleted the task :-)\n" + bcolors.ENDC)
+
 def todo_status(action, prev_state = None, new_state = None):
     display_tasks = [x_task for x_task in todos if x_task['STATUS'] == prev_state]
     if len(display_tasks) == 0:
-        sys.stdout.write("" + bcolors.FAIL + "NO TASKS to " + bcolors.FAIL + action + "\n")
+        sys.stdout.write("" + bcolors.FAIL + "NO TASKS to " + bcolors.FAIL + action + "\n" + bcolors.ENDC)
         exit()
-    counter, value = [], []
     todo_list(custom_list=display_tasks)
     try:
         mark_opt = int(raw_input("Which one? : "))
@@ -75,14 +84,14 @@ def todo_status(action, prev_state = None, new_state = None):
             sys.stdout.write("" + bcolors.FAIL + "This task number is not there :-(\n")
             exit()
     except ValueError as error:
+        sys.stdout.write("" + bcolors.FAIL + "This task number is not there :-(\n" + bcolors.ENDC)
         exit()
     mark_task = [task['NAME'] for cnt, task in enumerate(display_tasks) if cnt == mark_opt]
     for x_task in todos:
        if mark_task[0] == x_task['NAME']:
            x_task['STATUS'] = new_state
-           with open(source_file, 'w') as fp:
-               json.dump(todos, fp)
-           sys.stdout.write("" + bcolors.WARNING + "Successfully updated the task :-)\n")
+           save_tasks(todos)
+           sys.stdout.write("" + bcolors.WARNING + "Successfully updated the task :-)\n" + bcolors.ENDC)
            break
 
 def todo_print(todo, i):
@@ -105,14 +114,14 @@ def todo_print(todo, i):
 
 def todo_list(custom_list=None):
     if len(todos) == 0:
-         sys.stdout.write("\n" + bcolors.WARNING + "You have NO tasks!\n\n")
+         sys.stdout.write("\n" + bcolors.WARNING + "You have NO tasks!\n\n" + bcolors.ENDC)
          exit()
     if not custom_list:
         custom_list = todos
     for i in range(len(custom_list)):
         todo_print(custom_list[i], i)
     sys.stdout.write("\n")
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("action", type=str,
